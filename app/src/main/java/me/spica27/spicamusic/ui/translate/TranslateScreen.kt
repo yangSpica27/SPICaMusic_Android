24672e57d4a3ba7a319f39a5303df0a611743163
SPICa27 <1286939372@qq.com>
2025-09-01 11:37:56 +0800

    ## 修复 主题切换页面可能闪烁的问题

--- app/src/main/java/me/spica27/spicamusic/ui/translate/TranslateScreen.kt
+++ app/src/main/java/me/spica27/spicamusic/ui/translate/TranslateScreen.kt
@@ -1,6 +1,7 @@
 package me.spica27.spicamusic.ui.translate
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 import me.spica27.spicamusic.shaderrippleeffect.ExpandingWaveEffect
@@ -72,6 +74,9 @@ fun TranslateScreen(
                 modifier = Modifier
                   .fillMaxSize()
                   .padding(paddingValues)
+                  .pointerInput(Unit) {
+                    detectTapGestures {}
+                  }
               ) {
                 SettingPage()
               }
@@ -88,6 +93,9 @@ fun TranslateScreen(
                 modifier = Modifier
                   .fillMaxSize()
                   .padding(paddingValues)
+                  .pointerInput(Unit) {
+                    detectTapGestures {}
+                  }
               ) {
                 SettingPage()
               }
