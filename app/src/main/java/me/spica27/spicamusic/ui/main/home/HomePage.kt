47c9705966c065ff0b4ab94fffd7a36f0f36cec4
SPICa27 <1286939372@qq.com>
2025-10-03 21:29:01 +0800

    ## 修复 主页最近歌曲封面padding值错误

--- app/src/main/java/me/spica27/spicamusic/ui/main/home/HomePage.kt
+++ app/src/main/java/me/spica27/spicamusic/ui/main/home/HomePage.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -395,6 +396,7 @@ private fun OftenListenSongList(
             ),
         itemSpacing = 8.dp,
         preferredItemWidth = 150.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp),
     ) { index ->
         val item = songs[index]
         val coverPainter =
