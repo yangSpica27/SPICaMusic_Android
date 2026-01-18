59874ff309b83d03c2eddf68399cb8943d87f67f
SPICa27 <1286939372@qq.com>
2026-01-18 17:48:18 +0800

    新增歌词项和歌词 UI 组件，优化歌词查找逻辑，更新 .gitignore 文件

---
+++ app/src/main/java/me/spica27/spicamusic/ui/widget/LyricUI.kt
@@ -0,0 +1,10 @@
+package me.spica27.spicamusic.ui.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun LryricUI(modifier: Modifier = Modifier) {
+
+}
